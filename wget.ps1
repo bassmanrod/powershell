d762325894618aff254b4159dba26d63b7f78252
Rod Simioni <rodney@coastaldebt.com>
2024-09-26 15:12:37 -0400

    revised script

--- wget.ps1
+++ wget.ps1
@@ -1,22 +1,27 @@
-#New-Item -Path "c:\" -Name "temptemptemptemp" -ItemType Directory
 
-$folderPath = "C:\MyNewFolder5"
+$folderPath = "C:\scripts"
 
 if (-not (Test-Path -Path $folderPath)) {
 	    New-Item -Path $folderPath -ItemType Directory
-	        Write-Output "Folder Created Successfully!"
+	    Write-Output "Folder Created Successfully!"
 }
+
 else {
 	    Write-Output "Folder already exists!"
-	    exit
 }
 
-$url = "https://github.com/bassmanrod/scripts/blob/master/README.md"
+$url_a = "https://raw.githubusercontent.com/bassmanrod/powershell/refs/heads/master/deleteGoogleCache.ps1"
+$url_b = "https://raw.githubusercontent.com/bassmanrod/powershell/refs/heads/master/deleteCache.bat"
 
-$path_to_file = "$folderPath\goodness1.ps1"
+$path_to_file_a = "$folderPath\deleteGoogleCache.ps1"
+$path_to_file_b = "$folderPath\deleteCache.bat"
 
 Write-Output "Beginning fetch.";
-Invoke-RestMethod $url -OutFile $path_to_file
+Invoke-WebRequest $url_a -OutFile $path_to_file_a
+Invoke-WebRequest $url_b -OutFile $path_to_file_b
 Write-Output "Fetch ended.";
 
-Write-Output "Done."
+#Invoke-RestMethod $url_a -OutFile $path_to_file_a
+#Invoke-RestMethod $url_b -OutFile $path_to_file_b
+
+Write-Output "Done."`
